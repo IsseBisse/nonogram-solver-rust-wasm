fd4ae78d4690e778ef25cbfcad4d3cfed67d4575
Isak Liljequist <isak.liljequist@cgi.com>
2025-12-19 09:43:27 +0100

    Add explaination of hints format

--- example.js
+++ example.js
@@ -3,7 +3,8 @@ import init, { solve } from "../../solver-wasm/pkg/solver_wasm.js"
 const wasmReady = init();
 
 wasmReady.then(() => {
-    let hintsX = "2,2;4;1;2,1;1"
+    // Hints strings needs to have lines separated by semi-colon and "blocks" separated by commas
+    let hintsX = "2,2;4;1;2,1;1" 
     let hintsY = "1,2;2,1;1,1;2,1;2"    
     let dim = "5x5"
     
